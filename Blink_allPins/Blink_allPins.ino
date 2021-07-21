51a06de993010088733e3b233b99b215f7cd00c8
rsknox <rsknox@users.noreply.github.com>
2021-07-21 10:19:20 -0500

    added output pin declarations to blink all pins rtn

--- Blink_allPins/Blink_allPins.ino
+++ Blink_allPins/Blink_allPins.ino
@@ -25,6 +25,17 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 // the loop function runs over and over again forever
